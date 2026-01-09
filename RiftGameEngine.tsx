ccbb2e15647c453b4a2d031af70b4b6839cc9ac9
Pawel <42497752+pawelprusko@users.noreply.github.com>
2026-01-09 09:01:59 +0100

    Update RiftGameEngine.tsx

--- RiftGameEngine.tsx
+++ RiftGameEngine.tsx
@@ -701,7 +701,8 @@ export const RiftGameEngine = forwardRef<any, RiftEngineProps>(({ onComplete, sh
             ref={canvasRef}
             width={CANVAS_WIDTH}
             height={CANVAS_HEIGHT}
-            className="block w-full h-full object-contain touch-none"
+            // CHANGED: Use w-full h-full to stretch and fill, removing all black bars
+            className="block w-full h-full touch-none"
             style={{ imageRendering: 'pixelated' }}
         />
     );
